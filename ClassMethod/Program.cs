d1849b9f78918420c0f931da0e8965f7c2ef3134
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-12 03:51:50 +0530

    Add project files.

---
+++ ClassMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMethod
+{
+    class Vehicle
+    {
+        public string color;
+        public int NoOfWheels;
+    
+    }
+
+    class Method
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Vehicle();
+            v.color = "red";
+            v.NoOfWheels = 4;
+
+            Vehicle v2 = new Vehicle();
+            v2.color = "yellow";
+            v2.NoOfWheels = 2;
+
+            Console.WriteLine(v.color);
+            Console.WriteLine(v2.no);
+        }
+    }
+}
